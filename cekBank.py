a1f0187cef90f3c82d3309488ab3c9d875fef5b8
Muhammad Razan Rizqullah <akangozan007@gmail.com>
2024-04-19 20:01:11 +0700

    Update cekBank.py

--- cekBank.py
+++ cekBank.py
@@ -27,16 +27,28 @@ if inputanUser in pilihanmenu:
         for data in dataBank['data']:
             # print(data)
             # function untuk membongkar file json
-            print("Kode Bank "+ data['kodeBank'])
-            print("Kode Bank "+ data['namaBank'])
+            print("Kode Bank : "+ data['kodeBank'])
+            print("Nama Bank : "+ data['namaBank'])
             print("\n")
         
     elif inputanUser == 2:
+          #  Meminta Data User 
+        kodeBankUser = int(input('Masukkan Kode Bank            (Sesuaikan Dengan Opsi 1) : '))
+        RekBankUser = str(input('Masukkan Nomor Rekening Bank   (No Rekening mu)          : '))
         # Jika Opsi 2
-        pass
-    else:
-        # Jika tidak ada di dalam opsi
-        pass
+        url = f'https://api-rekening.lfourr.com/getBankAccount?bankCode={kodeBankUser}&accountNumber={RekBankUser}'
+        # Kirim permintaan GET
+        response = requests.get(url)
+        dataBank = response.json()
+        print("Status : " + dataBank['msg'])
+        if dataBank['status']: 
+            print("Kode Bank   : " + dataBank['data']['bankcode'])
+            print("Nama Bank   : " + dataBank['data']['bankname'])
+            print("No Rekening : " + dataBank['data']['accountnumber'])
+            print("Nama Pemilik: " + dataBank['data']['accountname'])
+            print("\n")
+        else:
+            print("Gagal mendapatkan data. Silakan coba lagi.")
 
 else:
     print("Pilihan mu tidak ada")
